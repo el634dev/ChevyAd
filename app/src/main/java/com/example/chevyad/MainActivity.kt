b89fd6a23c9e5858290e037b2bf78f1491d20ac0
el634dev <al7758066@gmail.com>
2026-01-29 12:16:53 -0500

    Added a switch and an icon

--- app/src/main/java/com/example/chevyad/MainActivity.kt
+++ app/src/main/java/com/example/chevyad/MainActivity.kt
@@ -11,14 +11,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.chevyad.ui.theme.ChevyAdTheme
 
@@ -59,6 +70,14 @@ fun ChevyAd(modifier: Modifier = Modifier) {
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                imageVector = Icons.Filled.Info,
+                contentDescription = "Information",
+                tint = Color.Gray,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(40.dp)
+            )
             Text(
                 text = "1965 Chevy Impala 409 Wagon",
                 textAlign = TextAlign.Center,
@@ -67,16 +86,35 @@ fun ChevyAd(modifier: Modifier = Modifier) {
                 modifier = modifier
             )
         }
+      BuyerSwitch()
     }
 }
 
-//@Composable
-//fun buyerSwitch(modifier: Modifier = Modifier) {
-//
-//}
+@Composable
+fun BuyerSwitch(modifier: Modifier = Modifier) {
+    var isChecked by remember { mutableStateOf(value = true) }
+    val message = if (isChecked) "Sold" else "For Sale"
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Switch(
+            checked = isChecked,
+            onCheckedChange = {
+                isChecked = it
+            }
+        )
+        Text(
+            text = message
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun ChevyAdPreview() {
-    ChevyAdTheme {}
+    ChevyAdTheme {
+        ChevyAd()
+    }
 }
